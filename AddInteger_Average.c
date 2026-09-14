28e7c2732f80e7326fa3b98d2c5fa1d46d3dcd8b
Rahat_404 <mahbuburrahmanrahat258@gmail.com>
2026-09-14 19:44:16 +0600

    Add files via upload

---
+++ AddInteger_Average.c
@@ -0,0 +1,16 @@
+#include<Stdio.h>
+int main (){
+    int a,b,sum;
+    printf ("Enter two intagers: \n");
+    scanf("%d %d", &a,&b);
+    sum=a+b;
+    printf ("Integers entered: %d,%d\n",a,b);
+    printf ("Sum of %d and %d is %d\n",a,b,sum);
+    float average=(float) sum/2.0;
+    printf ("Average of %d and %d is %.2f\n",a,b,average);
+
+         //sum and average of two integers
+    return 0;
+
+}
+
